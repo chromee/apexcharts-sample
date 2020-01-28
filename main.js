da0376b4cca0759e8c5e5e31ec2955c0b8152730
chromee <odk.mmmm@gmail.com>
2020-01-28 20:50:46 +0900

    get data from spreadsheet

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
-var chart = new ApexCharts(document.querySelector("#chart"), options);
-chart.render();
+let url = 'https://script.google.com/a/virtualcast.jp/macros/s/AKfycbxbe_dx4fbbMgkOViqhlVIPitm3qs4E-uy5iwR9tOw/dev'
+$.ajax({
+  type: 'GET',
+  url: url,
+  dataType: 'jsonp',
+  success: raw => {
+    let data = raw.map(v => { return { model_count: v[0], renderScale: v[1], fps: v[2] }; })
+
+    let modelsCounts = [...Array(20)].map((v, i) => i + 1);
+    let series = modelsCounts.map(modelsCount => {
+      let dataEachModelCount = data.filter(v => v.model_count == modelsCount)
+      return {
+        name: `model ${modelsCount}`,
+        data: dataEachModelCount.map(v => { return { x: v.renderScale * 100, y: v.fps } })
+      }
+    })
+    var chart = new ApexCharts(document.querySelector("#chart"), options(series));
+    chart.render();
+  }
+});
+
--- options.js
+++ options.js
@@ -1,50 +1,52 @@
-var options = {
-  chart: {
-    height: '95%',
-    type: 'heatmap',
-  },
-  series: series,
-  plotOptions: {
-    heatmap: {
-      colorScale: {
-        ranges: [{
-          from: -100,
-          to: 1,
-          color: '#eeeeee',
-          name: 'null',
+function options(series) {
+  return {
+    chart: {
+      height: '95%',
+      type: 'heatmap',
+    },
+    series: series,
+    plotOptions: {
+      heatmap: {
+        colorScale: {
+          ranges: [{
+            from: -100,
+            to: 1,
+            color: '#eeeeee',
+            name: 'null',
+          },
+          {
+            from: 1,
+            to: 45,
+            color: '#FFB200',
+            name: 'low (0~45 fps)',
+          },
+          {
+            from: 45,
+            to: 60,
+            color: '#128FD9',
+            name: 'medium (46~60 fps)',
+          },
+          {
+            from: 60,
+            to: 80,
+            color: '#00A100',
+            name: 'high (61~80 fps)',
+          }
+          ]
         },
-        {
-          from: 1,
-          to: 45,
-          color: '#FFB200',
-          name: 'low (0~45 fps)',
-        },
-        {
-          from: 45,
-          to: 60,
-          color: '#128FD9',
-          name: 'medium (46~60 fps)',
-        },
-        {
-          from: 60,
-          to: 80,
-          color: '#00A100',
-          name: 'high (61~80 fps)',
-        }
-        ]
-      },
-    }
-  },
-  title: {
-    text: 'frame rate each model count adn render viewport scale',
-    align: 'center',
-    style: {
-      fontSize: '26px'
-    }
-  },
-  grid: {
-    padding: {
-      right: 20
+      }
+    },
+    title: {
+      text: 'frame rate each model count adn render viewport scale',
+      align: 'center',
+      style: {
+        fontSize: '26px'
+      }
+    },
+    grid: {
+      padding: {
+        right: 20
+      }
     }
   }
-};
+}
